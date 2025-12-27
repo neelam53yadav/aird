25632e0f2f78e78a6e6b1f606dbb7fc9fbf50e99
Atul <atul@xtechstacks.com>
2025-12-28 00:07:37 +0530

    made the chnages

--- infra/terraform/iam.tf
+++ infra/terraform/iam.tf
@@ -32,16 +32,7 @@ resource "google_project_iam_member" "compute_instance_admin" {
   member  = "serviceAccount:${google_service_account.primedata_sa.email}"
 }
 
-# Service Account Key (for application use)
-# Note: Service account key creation may be disabled by organization policy
-# This resource will fail if key creation is disabled - that's expected
-resource "google_service_account_key" "primedata_sa_key" {
-  service_account_id = google_service_account.primedata_sa.name
-  public_key_type    = "TYPE_X509_PEM_FILE"
-
-  lifecycle {
-    # Keys are rotated, so allow replacement
-    create_before_destroy = true
-  }
-}
+# Note: Service account keys are intentionally not created here.
+# Service account key creation is disabled by organization policy (security best practice).
+# Applications should use Workload Identity Federation or the metadata service instead.
 
